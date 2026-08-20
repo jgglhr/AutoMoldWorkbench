6f619d00fe34afd9d10d882463c1477a7dea7172
jgglhr <jgglhr@gmail.com>
2026-08-20 13:09:03 -0300

    fix: improve logger formatting

--- automold/logger.py
+++ automold/logger.py
@@ -44,10 +44,27 @@ class AutoMoldLogger:
         self.level = level
 
     # --------------------------------------------------------------
+    # Internal formatting
+    # --------------------------------------------------------------
+
+    def _format_message(self, message, args):
+        """Format a logging message using the standard % style."""
+
+        if not args:
+            return str(message)
+
+        try:
+            return str(message) % args
+        except (TypeError, ValueError):
+            return " ".join(
+                [str(message)] + [str(arg) for arg in args]
+            )
+
+    # --------------------------------------------------------------
     # Internal output
     # --------------------------------------------------------------
 
-    def _write_console(self, level, message):
+    def _write_console(self, level, message, *args):
         """Write a message to the Python console."""
 
         if level not in self.LEVELS:
@@ -55,6 +72,8 @@ class AutoMoldLogger:
 
         if self.LEVELS[level] < self.LEVELS[self.level]:
             return
+
+        message = self._format_message(message, args)
 
         timestamp = datetime.datetime.now().strftime(
             "%H:%M:%S"
@@ -64,51 +83,56 @@ class AutoMoldLogger:
             "[AutoMold] "
             "[" + timestamp + "] "
             "[" + level + "] "
-            + str(message)
+            + message
         )
 
     # --------------------------------------------------------------
     # Logging methods
     # --------------------------------------------------------------
 
-    def debug(self, message):
+    def debug(self, message, *args):
         """Log a debug message."""
 
         self._write_console(
             "DEBUG",
             message,
+            *args,
         )
 
-    def info(self, message):
+    def info(self, message, *args):
         """Log an informational message."""
 
         self._write_console(
             "INFO",
             message,
+            *args,
         )
 
-    def warning(self, message):
+    def warning(self, message, *args):
         """Log a warning message."""
 
         self._write_console(
             "WARNING",
             message,
+            *args,
         )
 
-    def error(self, message):
+    def error(self, message, *args):
         """Log an error message."""
 
         self._write_console(
             "ERROR",
             message,
+            *args,
         )
 
-    def critical(self, message):
+    def critical(self, message, *args):
         """Log a critical message."""
 
         self._write_console(
             "CRITICAL",
             message,
+            *args,
         )
 
 
